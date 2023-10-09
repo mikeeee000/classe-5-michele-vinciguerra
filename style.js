ff51d9d776c9a3b3c2d22560ae8e985e63d212ef
michele vincigerra <michele-09@hotmail.it>
2023-10-09 11:42:38 +0200

    inizializzazione cartella

---
+++ style.js
@@ -0,0 +1,7 @@
+let list = [
+    { firstName: 'Marco', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+    { firstName: 'Simone', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+    { firstName: 'Matteo', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+    { firstName: 'Ettore', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+    ];
+    function list (){}
